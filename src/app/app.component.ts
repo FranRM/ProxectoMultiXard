1a1cf216bec2801c0557a3d97e4fd8af8ae361b9
Femio23 <chiscazo.paco@gmail.com>
2019-09-26 16:11:13 +0200

    Avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { Location } from '@angular/common';
+import {ActivatedRoute} from'@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  private location:Location;
   title = 'MultiXardApp';
-
-  benvida = 'Benvido a MultiXard';
+  volverBenvida(): void {
+    this.location.go('/benvida');
+  }
+  
 }
