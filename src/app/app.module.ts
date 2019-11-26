97262433b984f43a642f88b7c10fe7cc16c02b52
masterfabela <fabelaretorcida@gmail.com>
2019-11-26 13:49:31 +0100

    Implementado diseño angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { InterceptorTokenService } from '../app/servicios/interceptor-token.serv
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapaComponent } from './mapa/mapa.component';
+import { MaterialModule } from './shared/material.module';
 
 
 
@@ -33,7 +34,8 @@ import { MapaComponent } from './mapa/mapa.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [AutentificacionService,
    {
